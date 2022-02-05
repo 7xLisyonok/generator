aa966332da8cfcbf1d8b507b721774adffa9c353
7xLisyonok <7x.Lisyonok@gmail.com>
2022-02-05 21:22:48 +1000

    random int pair

--- src/Random.ts
+++ src/Random.ts
@@ -1,6 +1,24 @@
 export default class Random {
+    /**
+     * generate random int [from...to)
+     */
     static int(from: number, to: number) {
-        const len = to - from + 1;
+        const len = to - from;
         return Math.floor(Math.random() * len) + from;
+    }
+
+    /**
+     * generate two different(if its possible) random int [from...to)
+     */
+     static intPair(from: number, to: number) {
+        let newRandom1 = Random.int(from, to);
+        let newRandom2 = Random.int(from, to);
+
+        while (newRandom1 === newRandom2) {
+            if (to - from <= 1) break;
+            newRandom2 = Random.int(from, to);
+        }
+        
+        return [newRandom1, newRandom2];
     }
 }
