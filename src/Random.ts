00d7d027e9e47bf5a135af8e7768801ea6448709
7xLisyonok <7x.Lisyonok@gmail.com>
2022-02-06 17:00:23 +1000

    Random IntMixer

--- src/Random.ts
+++ src/Random.ts
@@ -21,4 +21,55 @@ export default class Random {
         
         return [newRandom1, newRandom2];
     }
+
+    /**
+     * create mixer with random ints [from...to)
+     */
+    static intMixer(from: number, to: number): IntMixer {
+        return new IntMixer(from, to);
+    }
+}
+
+
+
+class IntMixer {
+    private _arr: Array<number> = [];
+    private _length: number
+
+
+    constructor (
+        private _from: number, 
+        private _to: number
+    ) {
+        this._length = this._to - this._from;
+        this.createArray();
+
+        if (this._length  <= 0) {
+            throw new Error("\x1b[31m\x1b[1m IntMixer length <= 0 \x1b[0m");
+        }
+    }
+
+    private createArray() {
+        this._arr = Array.from({ length: this._length }, (_, i) => i + this._from);
+        
+        for(let i1 = 0; i1 < this._arr.length; i1++) {
+            const i2 = Random.int(0, this._arr.length);
+
+            const i1Val = this._arr[i1];
+            this._arr[i1] = this._arr[i2];
+            this._arr[i2] = i1Val;
+        }
+    }
+
+    has(count: number) {
+        return this._arr.length >= count;
+    }
+
+    readLoop(count: number): Array<number> {
+        if (this.has(count)) return this._arr.splice(-count);        
+        const result = this._arr;
+        this.createArray();
+        result.push(...this.readLoop(count - result.length));
+        return result;
+    }
 }
